433035cc8dbb3e54c1bc09c1fbe1fcb6fb85f10e
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2018-04-26 10:43:57 +0200

    Batch script

--- corrNetwork.bat
+++ corrNetwork.bat
@@ -8,16 +8,19 @@ SET PWD=%PWD:~0,-1%
 SET VENV_HOME=%PWD%/venv_win
 SET VENV_ACTIVE=%VENV_HOME%/Scripts/activate
 
-:: workflow variables
-:: TODO!!!! Interactive inputs
+:: input arguments
 REM SET PARAM_INFILE=S:\U_Proteomica\PROYECTOS\PESA_omicas\Results_tables_V1\Con_fraccionamiento\Clustering\PESA_V1_Proteinas_RF.xlsx
-SET PARAM_INFILE=D:\projects\corrNetwork\tests\test1-in.xlsx
-SET PARAM_METHOD=kendall
-SET PARAM_OUTFILE=D:\projects\corrNetwork\tests\test1-out_kendall.csv
+REM SET PARAM_INFILE=D:\projects\corrNetwork\tests\test1-in.xlsx
+REM SET PARAM_METHOD=kendall
+REM SET PARAM_OUTFILE=D:\projects\corrNetwork\tests\test1-out_kendall.csv
 
 :: interactive arguments
-REM SET /p PARAM_INFILE="Enter the input file (in XLSX format):"
-
+SET /p PARAM_INFILE="Enter the input file (in XLSX format): "
+SET /p PARAM_OUTFILE="Enter the output file (in CSV extension): "
+CHOICE /C PSK /N /T 10 /D P /M "Choice the method: Pearson [P], Spearman [S], Kendall [K] (Pearson by default)"
+IF ERRORLEVEL 1 SET PARAM_METHOD=pearson
+IF ERRORLEVEL 2 SET PARAM_METHOD=spearman
+IF ERRORLEVEL 3 SET PARAM_METHOD=kendall
 
 :: execute workflow
 CMD /k "%VENV_ACTIVE% && python %PWD%/correlation_network.py -i %PARAM_INFILE% -m %PARAM_METHOD% -o %PARAM_OUTFILE%"
